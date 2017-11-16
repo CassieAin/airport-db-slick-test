1d3c3a499a793fbbea9b9efe95975f94a1555695
CassieAin <roksolana.d22@gmail.com>
2017-11-16 08:20:18 +0200

    Trial version

--- src/main/scala/tasks/Queries.scala
+++ src/main/scala/tasks/Queries.scala
@@ -7,8 +7,8 @@ import models._
 import slick.jdbc.PostgresProfile.api._
 import utils.Main.formatter
 
-class Queries(db: Database) {
-
+object Queries {
+  val db = Database.forConfig("scalaxdb")
 
   def queryForTask63 = {
     val subquery = PassInTripTable.table.groupBy(p => (p.place, p.idPsgFk))
@@ -83,6 +83,7 @@ class Queries(db: Database) {
     db.run(query.result)
   }
 
+  /*!!!!!!!!!!!!!!!!!!!!*/
   def queryForTask95 = {
     val query = (for {
       c <- CompanyTable.table
@@ -93,6 +94,19 @@ class Queries(db: Database) {
      // .map { case ((c, t, pit), group) => (group.map(_._1.name).countDistinct, group.map(_._2.tripNo).countDistinct,
       //  group.map(_._2.plane).countDistinct, group.map(_._3.idPsgFk).countDistinct, group.length)}
 
+    db.run(query.result)
+  }
+
+  def queryForTask102 = {
+    val subquery = (for {
+      pit <- PassInTripTable.table
+      t <- TripTable.table if pit.tripNoFk === t.tripNo
+    } yield (pit, t))
+      .map{ case (pit, t) => ( pit, Case.If(t.townFrom <= t.townTo).Then(t.townFrom ++ t.townTo).Else(t.townFrom ++ t.townTo) )}
+      .groupBy(_._1.idPsgFk)
+      // .filter { case (pit, t1) => ( t1.map(_._2).countDistinct === 1)}
+      .map(_._1)
+    val query = PassengerTable.table.filter(_.idPsg in subquery).map(_.name)
     db.run(query.result)
   }
 
@@ -116,16 +130,17 @@ class Queries(db: Database) {
       pit <- PassInTripTable.table if t.tripNo === pit.tripNoFk
     } yield (c, t, pit))
       .filter { case (c, t, pit) => (t.townFrom === "Rostov") && (t.timeOut <= date) }
-      .map { case (c, t, pit) => (t.timeOut, pit.idPsgFk, c.name, t.tripNo) }
-      .sortBy { case (date, pid, name, tripNo) => (date, pid) }
-      .take(5)
-      .drop(4)
+        .map { case (c, t, pit) => (t.timeOut, pit.idPsgFk, c.name, t.tripNo) }
+          .sortBy { case (date, pid, name, tripNo) => (date, pid) }
+            .take(5)
+              .drop(4)
 
     val query = subquery.map { case (date, pid, name, tripNo) => (date, name, tripNo) }
 
     db.run(query.result)
   }
 
+  /*!!!!!!!!!!!!!!!!!!!!*/
   def queryForTask110 = {
 
     def days(t1: LocalDateTime, t2: LocalDateTime) = ChronoUnit.DAYS.between(t1, t2)
@@ -139,5 +154,41 @@ class Queries(db: Database) {
 
     val query = PassengerTable.table.filter(_.idPsg in subquery).map(_.name)
     db.run(query.result)
+
+    /*
+
+   select name from passenger where id_psg in
+      (select id_psg
+       from pass_in_trip pit join trip t on pit.trip_no = t.trip_no
+       where time_in < time_out and datepart(dw, date) = 7
+      )
+    */
+  }
+
+  /*!!!!!!!!!!!!!!!!!!!!*/
+  def queryForTask114 = {
+    val b = PassInTripTable.table
+      .groupBy{ case pit => (pit.idPsgFk, pit.place)}
+      .map{ case (id, place) => (id, place.length)}
+    val bmax = b.map(_._2) //.max
+    val b1 = b.filter{ case (id, count) => (count === count)}
+      .map{ case (id, count) => (id._1, count)}
+
+
+    val query = (for {
+      p <- PassengerTable.table
+      b1 <- b1 if p.idPsg === b1._1
+    } yield (p, b1))
+      .map{ case (p, b1) => (p.name, b1._2)}
+
+    db.run(query.result)
+
+    /*
+      WITH b AS
+      (SELECT ID_psg, COUNT(*) as cnt FROM Pass_In_Trip GROUP BY ID_psg, place),
+      b1 AS
+        (SELECT DISTINCT ID_psg, cnt FROM b WHERE cnt =(SELECT MAX(cnt) FROM b))
+      SELECT name, cnt FROM b1 JOIN Passenger p ON (b1.ID_psg = p.ID_psg)
+    */
   }
 }
